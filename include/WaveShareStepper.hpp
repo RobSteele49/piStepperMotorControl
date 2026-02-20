2a49017c37a17ea74320063365b23b664b74641c
Rob Steele <robsteele49@gmail.com>
2026-02-19 19:45:29 -0800

    Update from Gemini

--- include/WaveShareStepper.hpp
+++ include/WaveShareStepper.hpp
@@ -2,16 +2,15 @@
  * Project:   LX200 Focuser Automation
  * Component: WaveShare Stepper Driver Interface (Header)
  * Author:    Robert D. Steele
- * Date:      2026-02-18
- * Version:   1.6
- * Copyright (c) 2026 Robert D. Steele. All Rights Reserved.
+ * Date:      2026-02-19
+ * Version:   2.4 (Cleaned)
  */
 
 #ifndef WAVESHARE_STEPPER_HPP
 #define WAVESHARE_STEPPER_HPP
 
 #include <pigpio.h>
-#include "config.h" // Motor configuration file
+#include "config.h" // Includes Direction enum and gear ratios
 
 enum MotorChannel { MOTOR_1, MOTOR_2 };
 
@@ -20,30 +19,27 @@ public:
     WaveShareStepper(MotorChannel channel);
     ~WaveShareStepper();
 
+    // Power and Emergency Control
     void setPower(bool on);
     void stop();
-    
-    void moveAtHz(int frequency, Direction dir);           // Non-blocking (Hardware PWM)
-    void moveSteps(int steps, int speedHz, Direction dir);   // Blocking
-    void moveStepsRamped(int totalSteps, int targetHz, int rampTimeMs, Direction dir); 
-    void moveTo(long long targetPosition, int speedHz);      // Absolute move
-    void moveRelative(long long offset, int speedHz);        // Relative move
+    static void globalEmergencyStop(WaveShareStepper* m1, WaveShareStepper* m2 = nullptr);
 
-    void reSeat(); // Add this line in the public: section
-  
-    void moveRamped(int targetHz, int rampTimeMs, Direction dir);
-    void stopRamped(int rampTimeMs);
+    // Basic Movement
+    void moveAtHz(int frequency, Direction dir);
+    void moveSteps(int steps, int speedHz, Direction dir);
+    void moveRelative(long long offset, int speedHz);
 
-    long long getPosition() const { return _stepPosition; }
+    // Advanced Precision Movement
+    void moveStepsRamped(int totalSteps, int targetHz, int rampTimeMs, Direction dir);
+    void moveTo(long long targetPosition, int speedHz); // Handles Backlash automatically
+    void reSeat();                                     // Fixes Mirror Flop
+
+    // Status Getters/Setters
+    long long getCurrentPosition();
     void setCurrentPosition(long long pos) { _stepPosition = pos; }
     int getCurrentHz() const { return _currentHz; }
     bool isMoving() const { return _currentHz > 0; }
 
-    long long getCurrentPosition();
-  
-    // Static method: can be called without an object to kill everything
-    static void globalEmergencyStop(WaveShareStepper* m1, WaveShareStepper* m2 = nullptr);
-  
 private:
     int _en, _dir, _step;
     bool _is_enabled = false;
