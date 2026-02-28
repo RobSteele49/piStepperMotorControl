05a0074245f3923fc84f78682586f64f7f071d39
Rob Steele <robsteele49@gmail.com>
2026-02-28 10:52:42 -0800

    Starting to get comm working.

--- include/AlpacaServer.hpp
+++ include/AlpacaServer.hpp
@@ -4,7 +4,7 @@
  * File:       AlpacaServer.hpp
  * Author:     Robert D. Steele
  * Date:       2026-02-23
- * Version:    2.0 (Update 2/28/26 from Gemini)
+ * Version:    2.1 (Update 2/28/26 from Gemini - Case Insensitivity Fix)
  * Copyright (c) 2026 Robert D. Steele. All Rights Reserved.
  */
 
@@ -51,54 +51,87 @@ private:
             res.set_content("{\"Value\":[{\"DeviceName\":\"Dual Controller Focuser\",\"DeviceType\":\"Focuser\",\"DeviceNumber\":0,\"UniqueID\":\"pi-foc-01\"},{\"DeviceName\":\"Dual Controller Rotator\",\"DeviceType\":\"Rotator\",\"DeviceNumber\":0,\"UniqueID\":\"pi-rot-01\"}],\"ClientTransactionID\":0,\"ServerTransactionID\":1,\"ErrorNumber\":0,\"ErrorMessage\":\"\"}", "application/json");
         });
 
-        // --- FOCUSER MANDATORY ROUTES ---
-        svr.Get("/api/v1/focuser/0/connected", [&](const httplib::Request& req, httplib::Response& res) {
+        // --- FOCUSER MANDATORY ROUTES (With Case-Insensitivity Aliases) ---
+        
+        // Connected Property
+        auto foc_conn = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatBoolResponse(true, req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/focuser/0/connected", foc_conn);
+        svr.Get("/api/v1/focuser/0/Connected", foc_conn);
+        svr.Put("/api/v1/focuser/0/connected", foc_conn);
+        svr.Put("/api/v1/focuser/0/Connected", foc_conn);
 
-        svr.Get("/api/v1/focuser/0/absolute", [&](const httplib::Request& req, httplib::Response& res) {
+        // Absolute Property
+        auto foc_abs = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatBoolResponse(true, req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/focuser/0/absolute", foc_abs);
+        svr.Get("/api/v1/focuser/0/Absolute", foc_abs);
 
-        svr.Get("/api/v1/focuser/0/maxstep", [&](const httplib::Request& req, httplib::Response& res) {
-            res.set_content(formatResponse(FOC_LIMIT_MAX, req), "application/json");
-        });
-
-        svr.Get("/api/v1/focuser/0/position", [&](const httplib::Request& req, httplib::Response& res) {
+        // Position Property
+        auto foc_pos = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatResponse(focuser->getCurrentPosition(), req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/focuser/0/position", foc_pos);
+        svr.Get("/api/v1/focuser/0/Position", foc_pos);
 
-        svr.Get("/api/v1/focuser/0/ismoving", [&](const httplib::Request& req, httplib::Response& res) {
-            // Future improvement: check if motor is actually stepping
+        // MaxStep Property
+        auto foc_max = [&](const httplib::Request& req, httplib::Response& res) {
+            res.set_content(formatResponse(FOC_LIMIT_MAX, req), "application/json");
+        };
+        svr.Get("/api/v1/focuser/0/maxstep", foc_max);
+        svr.Get("/api/v1/focuser/0/MaxStep", foc_max);
+
+        // IsMoving Property
+        auto foc_mov = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatBoolResponse(false, req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/focuser/0/ismoving", foc_mov);
+        svr.Get("/api/v1/focuser/0/IsMoving", foc_mov);
 
-        svr.Put("/api/v1/focuser/0/move", [&](const httplib::Request& req, httplib::Response& res) {
+        // Move Command
+        auto foc_move_cmd = [&](const httplib::Request& req, httplib::Response& res) {
             if (req.has_param("Position")) {
                 long long target = std::stoll(req.get_param_value("Position"));
                 std::cout << "[NET] Focuser Move -> " << target << std::endl;
                 focuser->moveTo(target, FOC_SPEED_MED);
             }
             res.set_content(formatResponse(0, req), "application/json");
-        });
+        };
+        svr.Put("/api/v1/focuser/0/move", foc_move_cmd);
+        svr.Put("/api/v1/focuser/0/Move", foc_move_cmd);
 
-        // --- ROTATOR MANDATORY ROUTES ---
-        svr.Get("/api/v1/rotator/0/connected", [&](const httplib::Request& req, httplib::Response& res) {
+
+        // --- ROTATOR MANDATORY ROUTES (With Case-Insensitivity Aliases) ---
+
+        // Connected Property
+        auto rot_conn = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatBoolResponse(true, req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/rotator/0/connected", rot_conn);
+        svr.Get("/api/v1/rotator/0/Connected", rot_conn);
+        svr.Put("/api/v1/rotator/0/connected", rot_conn);
+        svr.Put("/api/v1/rotator/0/Connected", rot_conn);
 
-        svr.Get("/api/v1/rotator/0/position", [&](const httplib::Request& req, httplib::Response& res) {
+        // Position Property
+        auto rot_pos = [&](const httplib::Request& req, httplib::Response& res) {
             res.set_content(formatResponse(rotator->getCurrentPosition(), req), "application/json");
-        });
+        };
+        svr.Get("/api/v1/rotator/0/position", rot_pos);
+        svr.Get("/api/v1/rotator/0/Position", rot_pos);
 
-        svr.Put("/api/v1/rotator/0/moveabsolute", [&](const httplib::Request& req, httplib::Response& res) {
+        // MoveAbsolute Command
+        auto rot_move_cmd = [&](const httplib::Request& req, httplib::Response& res) {
             if (req.has_param("Position")) {
                 long long target = std::stoll(req.get_param_value("Position"));
                 std::cout << "[NET] Rotator Move -> " << target << std::endl;
                 rotator->moveTo(target, ROT_SPEED_MED);
             }
             res.set_content(formatResponse(0, req), "application/json");
-        });
+        };
+        svr.Put("/api/v1/rotator/0/moveabsolute", rot_move_cmd);
+        svr.Put("/api/v1/rotator/0/MoveAbsolute", rot_move_cmd);
 
         std::cout << "\n[SUCCESS] Alpaca Server active on http://192.168.5.11:" << port << std::endl;
         
